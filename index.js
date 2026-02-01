577e07f3b6e5ea3b9beb6604ed682009a0f85e40
Savyez <visnuk252@gmail.com>
2026-02-01 20:46:17 +0530

    Add homepage routing

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-// HINTS:
+import express from 'express';
+import axios from 'axios';
+import path from 'path';
+import { fileURLToPath } from 'url';
 // 1. Import express and axios
 
-// 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
 
-// 3. Use the public folder for static files.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// 4. When the user goes to the home page it should render the index.ejs file.
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
 
-// 6. Listen on your predefined port and start the server.
+// Home route to fetch and display a random secret
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://secrets-api.appbrewery.com/random');
+    const secretData = response.data;
+    res.render(__dirname + '/views/index.ejs', {
+        secret: secretData.secret,
+        username: secretData.username
+    });
+  } catch (error) {
+    res.status(500).send('Error fetching secret');
+  }
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
